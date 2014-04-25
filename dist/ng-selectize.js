44fdf403fefce5cefe0251f720e4438e636cf6d1
Ross Rogers <ross.rogers@gmail.com>
2014-04-25 10:32:50 -0700

    Adding the ability to watch a `$scope` field to add options into the selectize widget.

--- dist/ng-selectize.js
+++ dist/ng-selectize.js
@@ -1,14 +1,34 @@
-(function() {
-  "use strict";
+"use strict";
 
-  angular.module("theaquaNg", ['ng']).directive("selectize", function($timeout) {
-    return {
-      restrict: "AE",
-      link: function(scope, element, attrs) {
-        return $timeout(function() {
-          return $(element).selectize(scope.$eval(attrs.selectize));
-        });
+angular.module('clientApp').directive("selectize", ['$timeout',function($timeout) {
+  return {
+    restrict: "AE",
+    link: function(scope, element, attrs) {
+      
+      var selectize_inst = null
+      
+      var load_angularjs_data = function() {
+        if (selectize_inst === null || !('selectizeData' in attrs)) { 
+          return
+        }
+        var data_array = scope.$eval(attrs.selectizeData)
+        if (!$.isArray(data_array)) {
+          return
+        }
+        for (var i in data_array) {
+          selectize_inst.addOption(data_array[i])
+        }
       }
-    };
-  });
-}).call(this);
+      
+      if ('selectizeData' in attrs) {
+        scope.$watch(attrs.selectizeData,load_angularjs_data)
+      }
+      return $timeout(function() {
+        var result = $(element).selectize(scope.$eval(attrs.selectize))
+        selectize_inst = result[0].selectize
+        load_angularjs_data()
+        return result
+      });
+    }
+  };
+}]);
